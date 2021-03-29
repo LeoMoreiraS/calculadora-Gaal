38771cf8ad22103c3cba94e80fe2783b2ae87d9a
Leonardo Moreira <leomoreira.com@gmail.com>
2021-03-29 14:00:36 -0300

    Adicionado evento de click no form com target no botão retornando tipo de operação

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -2,11 +2,25 @@ function criaCalculadora(){
     return{
     oper : document.querySelector('.op-list'),
     form : document.querySelector('.input-calc'),
-    teste(){
-        console.log(this.oper,this.form);
+    inicia(){
+        this.clickDoBotao();
+        console.log(this.form);
+    },
+    clickDoBotao(){
+        this.form.addEventListener('click',(e) =>{
+            e.preventDefault();
+            let evntrgt = e.target;
+            if(evntrgt.classList.contains('btn-op')){
+                this.teste(this.oper.value);
+            }
+
+        });
+    },
+    teste(a){
+       console.log(a);
     }
     };
 }
 
 const calculadora = criaCalculadora();
-calculadora.teste();
+calculadora.inicia();
