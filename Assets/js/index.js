f84e88bb5fcf34ac0911e1a2b2877e311dd42afa
Leonardo Moreira <leomoreira.com@gmail.com>
2021-03-29 14:26:53 -0300

    adicionado array de operacoes baseado no value da op

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -2,16 +2,30 @@ function criaCalculadora(){
     return{
     oper : document.querySelector('.op-list'),
     form : document.querySelector('.input-calc'),
+    normavet(){
+        console.log('oi');
+    },
+    prodVet(){
+        console.log('ola');
+    },
     inicia(){
-        this.clickDoBotao();
+        operacoes = [
+            this.normavet,
+            this.prodVet
+        ];
+        this.clickDoBotao(operacoes);
         console.log(this.form);
     },
-    clickDoBotao(){
+    
+    clickDoBotao(operacoes){
+        
         this.form.addEventListener('click',(e) =>{
             e.preventDefault();
             let evntrgt = e.target;
             if(evntrgt.classList.contains('btn-op')){
+                let op = this.oper.value;
                 this.teste(this.oper.value);
+                operacoes[op]();
             }
 
         });
