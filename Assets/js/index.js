715320d340bf03b64f030b23e7bf44e4e26b9351
Leonardo Moreira <leomoreira.com@gmail.com>
2021-03-31 21:24:16 -0300

    Removido botão e funcionalidade para escolher direto na select

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -7,7 +7,7 @@ function criaCalculadora() {
                 this.normavet,
                 this.prodVet
             ];
-            this.clickDoBotao(operacoes);
+            this.eventos(operacoes);
         },
         tamanhoVet(){
             let existe = this.form.querySelector(".tam");
@@ -15,20 +15,21 @@ function criaCalculadora() {
                 let tam = document.createElement('div');
                 let sel = document.createElement('select');
                 tam.classList.add("tam");
-                sel.classList.add("sel");
-                for(i=0;i<10;i++){
+                sel.classList.add("tam-list");
+                let deflt = document.createElement('option');
+                deflt.selected = 'true';
+                deflt.disabled = 'true';
+                deflt.style = "display: none";
+                deflt.innerText = 'escolha o tamanho';
+                sel.appendChild(deflt);
+                
+                for(i=0;i<5;i++){
                     let a = document.createElement('option');
                     a.attributes.value=i;
                     a.text = i+2;
                     sel.appendChild(a);
                 }
                 tam.appendChild(sel);
-                let btn =  document.createElement('button');
-                btn.textContent = 'Tamanho';
-                btn.classList.add('btn-tam');
-                btn.classList.add('btn');
-                btn.classList.add('btn-primary');
-                tam.appendChild(btn);
                 this.form.appendChild(tam);
             }else{
                 existe.remove();
@@ -46,18 +47,21 @@ function criaCalculadora() {
                 let entrada= document.createElement('form');
                 entrada.classList.add('entrada');
                 entrada.appendChild(brk);
+                //vetor 1
                 for(i=0;i<tamanho;i++){
                     
                     let vet1 = document.createElement('input');
-                    vet1.id = i;
+                    vet1.name = i;
                     entrada.appendChild(vet1);
                 }
+                //vetor 2
                 entrada.appendChild(brk);
                 for(i=0;i<tamanho;i++){
                     let vet2 = document.createElement('input');
-                    vet2.id = tamanho+i;
+                    vet2.name = parseInt(tamanho)+i;
                     entrada.appendChild(vet2);
                 }
+                //botao de calculo
                 let butao = document.createElement('button');
                 butao.classList.add('btn-calc');
                 butao.classList.add('btn');
@@ -72,40 +76,63 @@ function criaCalculadora() {
 
             
         },
-        normavet(tamanho,op) {
-            console.log(tamanho + ' ' + op);
+        pegaVet(tamanho){
+            v = document.querySelector('.entrada');
+            vetores = v.querySelectorAll('input');
+            let vets = new Array();
+            console.log(vetores);
+            let v1 = new Array();
+            let v2 = new Array();
+            for(i=0;i<tamanho/2;i++){
+                console.log(vetores[i].value);
+                //let num = vetores[i];
+                //v1.push(num.value);
+            }
+            vets.push(v1);
+            for(i=tamanho/2;i<tamanho;i++){
+                console.log(vetores[i].value);
+              //  let num = vetores[i];
+               // v2.push(num.value);
+            }
+            vets.push(v2);
+            return vets;
         },
-        prodVet(tamanho,op) {
-            console.log(tamanho + ' ' + op);
+        normavet(tamanho,op,vets) {
+            console.log(vets);            
         },
-
-        clickDoBotao(operacoes) {
+        prodVet(tamanho,op,vets) {
+            console.log(vets);
+            
+        },
+        
+        eventos(operacoes) {
 
             this.form.addEventListener('click', (e) => {
                 e.preventDefault();
                 let evntrgt = e.target;
-                console.log(evntrgt);
-                if (evntrgt.classList.contains('btn-op')) {
+                if(evntrgt.classList.contains('btn-calc')){
                     let op = this.oper.value;
-                    this.teste(this.oper.value);
+                    let tamanho = document.querySelector('.tam-list');
+                    
+                    let vets = (this.pegaVet(tamanho.value));
+                    operacoes[op](tamanho.value,op,vets);
+                }
+            });
+            this.form.addEventListener('change', (e)=>{
+                let evntrgt = e.target;
+                if(evntrgt.classList.contains('op-list')){
+                    let op = this.oper.value;
                     if(op>= 0 && op<=2){
                         this.tamanhoVet();
                     }
-                    
                 }
-                if (evntrgt.classList.contains('btn-tam')){
-                    
-                    let tamanho = document.querySelector('.sel');
-                    this.alocaTamVet(tamanho.value);
-                    //
-                    
-                }
-                if(evntrgt.classList.contains('btn-calc')){
-                    let op = this.oper.value;
-                    let tamanho = document.querySelector('.sel');
-                    operacoes[op](tamanho.value,op);
+                if (evntrgt.classList.contains('tam-list')) {
+                        let tamanho = document.querySelector('.tam-list');
+                        console.log(tamanho.value);
+                        this.alocaTamVet(tamanho.value);
                 }
             });
+
         },
         teste(a) {
             console.log(a);
